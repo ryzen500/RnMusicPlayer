b403510f770137f660b33f3ac1999de2193a6ca4
ameld <73698073+amel1210@users.noreply.github.com>
2022-03-25 16:10:00 +0700

    db(nambah field)

---
+++ db/update-datetime-artistReport.sql
@@ -0,0 +1,392 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 25, 2022 at 10:06 AM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `slotify`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(250) NOT NULL,
+  `firstName` varchar(70) NOT NULL,
+  `lastName` varchar(70) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `signUpDate` datetime NOT NULL,
+  `profilePic` varchar(500) NOT NULL,
+  `descripsi` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `firstName`, `lastName`, `email`, `password`, `signUpDate`, `profilePic`, `descripsi`) VALUES
+(1, 'mencoba', 'Ryzen', '500', 'Ryzen500@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', '2021-02-12 00:00:00', 'assets/images/profile-pics/head_emerald.png', 'Keep Moving Forward');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `albums`
+--
+
+CREATE TABLE `albums` (
+  `id` int(11) NOT NULL,
+  `title` varchar(250) NOT NULL,
+  `artist` int(11) NOT NULL,
+  `genre` int(11) NOT NULL,
+  `artworkPath` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `albums`
+--
+
+INSERT INTO `albums` (`id`, `title`, `artist`, `genre`, `artworkPath`) VALUES
+(1, 'MOMOLAND X CHROME ALBUMS', 2, 4, 'assets/images/artwork/popdance.jpg'),
+(2, 'NEFFEX ALBUMS', 5, 1, 'assets/images/artwork/neffex.jpg'),
+(3, 'Bensound', 6, 4, 'assets/images/artwork/popdance.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `artists`
+--
+
+CREATE TABLE `artists` (
+  `id` int(11) NOT NULL,
+  `name` varchar(250) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `artists`
+--
+
+INSERT INTO `artists` (`id`, `name`, `date`) VALUES
+(1, 'Eminem', '2020-11-10'),
+(2, 'MOMOLAND X CHROMANCE', '2018-09-03'),
+(3, 'Alan Walker', '2021-03-17'),
+(4, 'JFLa', '2020-02-13'),
+(5, 'NEFFEX', '2018-03-05'),
+(6, 'Bensound', '2016-09-08');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `genres`
+--
+
+CREATE TABLE `genres` (
+  `id` int(11) NOT NULL,
+  `name` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `genres`
+--
+
+INSERT INTO `genres` (`id`, `name`) VALUES
+(1, 'Rap'),
+(2, 'Podcast'),
+(3, 'Jazz'),
+(4, 'Pop'),
+(5, 'R & B'),
+(6, 'K-Pop'),
+(7, 'Metal'),
+(8, 'Hip-Hop'),
+(9, 'Rock'),
+(11, 'Country');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `message`
+--
+
+CREATE TABLE `message` (
+  `id` int(11) NOT NULL,
+  `nama` int(11) NOT NULL,
+  `pesan` varchar(255) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `tanggal` date NOT NULL,
+  `jam` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `playlists`
+--
+
+CREATE TABLE `playlists` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `owner` varchar(50) NOT NULL,
+  `dateCreated` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `playlists`
+--
+
+INSERT INTO `playlists` (`id`, `name`, `owner`, `dateCreated`) VALUES
+(3, 'Playlist Music One', 'ryzen500', '2021-08-24 00:00:00'),
+(4, 'Playlist Music Two', 'ryzen500', '2021-08-24 00:00:00'),
+(5, 'Playlist 3', 'ryzen500', '2021-08-30 00:00:00'),
+(6, 'Playlisst 4', 'ryzen500', '2021-11-02 00:00:00'),
+(7, 'Tester', 'ryzen500', '2021-12-23 00:00:00'),
+(8, 'Asade', 'Opet1', '2022-01-04 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `playlistsongs`
+--
+
+CREATE TABLE `playlistsongs` (
+  `id` int(11) NOT NULL,
+  `songId` int(11) NOT NULL,
+  `playlistId` int(11) NOT NULL,
+  `playlistOrder` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `playlistsongs`
+--
+
+INSERT INTO `playlistsongs` (`id`, `songId`, `playlistId`, `playlistOrder`) VALUES
+(29, 1, 4, 0),
+(30, 3, 5, 0),
+(32, 3, 3, 0),
+(33, 2, 3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `songs`
+--
+
+CREATE TABLE `songs` (
+  `id` int(11) NOT NULL,
+  `title` varchar(250) NOT NULL,
+  `artist` int(11) DEFAULT NULL,
+  `album` int(11) DEFAULT NULL,
+  `genre` int(11) DEFAULT NULL,
+  `duration` varchar(8) NOT NULL,
+  `path` varchar(500) NOT NULL,
+  `albumOrder` int(11) DEFAULT NULL,
+  `plays` int(11) DEFAULT NULL,
+  `datetime` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `songs`
+--
+
+INSERT INTO `songs` (`id`, `title`, `artist`, `album`, `genre`, `duration`, `path`, `albumOrder`, `plays`, `datetime`) VALUES
+(32, 'NEFFEX - How s It Supposed To Feel [Copyright Free]', 5, 2, 1, '03:05', 'assets/music/How s It Supposed To Feel.mp3', 0, 1, '2022-02-21'),
+(33, 'lala', 5, 2, 1, '03:05', 'assets/music/How s It Supposed To Feel.mp3', 0, 1, '2022-03-21');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(250) NOT NULL,
+  `firstName` varchar(70) NOT NULL,
+  `lastName` varchar(70) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `signUpDate` datetime NOT NULL,
+  `profilePic` varchar(500) NOT NULL,
+  `status` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `firstName`, `lastName`, `email`, `password`, `signUpDate`, `profilePic`, `status`) VALUES
+(1, 'Lala', ' Lastri ', 'Lalass', 'Lala@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', '2021-02-12 00:00:00', 'assets/images/profile-pics/head_emerald.png', 'active'),
+(10, 'ryzen500', 'Achmad', 'Tsany', 'Ahmadtsani145@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', '2022-03-04 00:00:00', 'assets/images/profile-pics/head_emerald.png', 'Active');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `albums`
+--
+ALTER TABLE `albums`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `artist` (`artist`),
+  ADD KEY `genre` (`genre`);
+
+--
+-- Indexes for table `artists`
+--
+ALTER TABLE `artists`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `genres`
+--
+ALTER TABLE `genres`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `nama` (`nama`);
+
+--
+-- Indexes for table `playlists`
+--
+ALTER TABLE `playlists`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `playlistsongs`
+--
+ALTER TABLE `playlistsongs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `songId` (`songId`),
+  ADD KEY `playlistId` (`playlistId`);
+
+--
+-- Indexes for table `songs`
+--
+ALTER TABLE `songs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `artist` (`artist`),
+  ADD KEY `album` (`album`),
+  ADD KEY `genre` (`genre`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `albums`
+--
+ALTER TABLE `albums`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `artists`
+--
+ALTER TABLE `artists`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `genres`
+--
+ALTER TABLE `genres`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `message`
+--
+ALTER TABLE `message`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `playlists`
+--
+ALTER TABLE `playlists`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `playlistsongs`
+--
+ALTER TABLE `playlistsongs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
+-- AUTO_INCREMENT for table `songs`
+--
+ALTER TABLE `songs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `albums`
+--
+ALTER TABLE `albums`
+  ADD CONSTRAINT `albums_ibfk_1` FOREIGN KEY (`artist`) REFERENCES `artists` (`id`),
+  ADD CONSTRAINT `albums_ibfk_2` FOREIGN KEY (`genre`) REFERENCES `genres` (`id`);
+
+--
+-- Constraints for table `message`
+--
+ALTER TABLE `message`
+  ADD CONSTRAINT `message_ibfk_1` FOREIGN KEY (`nama`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `songs`
+--
+ALTER TABLE `songs`
+  ADD CONSTRAINT `songs_ibfk_1` FOREIGN KEY (`album`) REFERENCES `albums` (`id`),
+  ADD CONSTRAINT `songs_ibfk_2` FOREIGN KEY (`artist`) REFERENCES `artists` (`id`),
+  ADD CONSTRAINT `songs_ibfk_3` FOREIGN KEY (`genre`) REFERENCES `genres` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
